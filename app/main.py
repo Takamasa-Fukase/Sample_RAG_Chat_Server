26447d95e92004f7d408861899e3fbb130742aa2
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-23 17:50:56 +0900

    Flutter側からの疎通確認用のPING APIを追加。Flutter側と疎通確認できたところまで。

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+import json, threading
+from typing import List, Optional
+from fastapi import FastAPI, Request, HTTPException
+from starlette.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from sse_starlette import EventSourceResponse
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "どうも、ウルトラ深瀬です"}
+# CORSを回避するために追加
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/ping')
+def ping():
+    return {'data': {'message': 'OK'}}
+
+# @app.post('/chat')
+# def get_answer():
