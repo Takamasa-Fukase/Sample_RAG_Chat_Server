0ffc55abe882f886e176097a8f1ce3101ac10b97
Takamasa-Fukase <ultrafukase@gmail.com>
2025-03-09 23:38:22 +0900

    DB=>Python=>Flutter間での改行コードのエスケープ問題の暫定対応。自己紹介テキストの改行が正常に表示されるようにした。

--- app/category_repository.py
+++ app/category_repository.py
@@ -9,4 +9,7 @@ class CategoryRepository:
     ) -> List[Category]:
         category_orms = db.query(CategoryORM).all()
         categories = [Category.from_orm(orm) for orm in category_orms]
+        # TODO: 後で色々考え直したい。暫定処理。改行コードのエスケープ関連。
+        for category in categories:
+            category.introduction_text = category.introduction_text.replace("\\n", "\n")
         return categories
